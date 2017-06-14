e28c568959ad4c8fbb8fedd4c45794d07288ed83
Johann Schmitz <johann@j-schmitz.net>
2017-06-14 06:19:13 +0200

    Tests for read_string

--- tests/test_format.py
+++ tests/test_format.py
@@ -22,6 +22,9 @@ class BytesMock(object):
     def __getattr__(self, item):
         return getattr(os, item)
 
+# junk is used to avoid false asserts because .read(n) reads *at most* n bytes and doesn't complain
+# if fewer bytes are read
+junk = b'\x01' * 20
 
 class TestFormat(object):
     
@@ -42,10 +45,17 @@ class TestFormat(object):
         self._test_read_number(b'\xff\xff\xff\x01\xab\xcd\xef', 28036591)
 
     def _test_read_number(self, raw_bytes, expected_number):
-        # junk is used to avoid false asserts because .read(n) reads *at most* n bytes and doesn't complain
-        # if fewer bytes are read
-        junk = b'\x01' * 20
 
         print("read_number - bytes: %s, expected: %s" % (binascii.hexlify(raw_bytes), expected_number))
         with mock.patch('peix.format.os', new=BytesMock(raw_bytes + junk)):
             assert EixFileFormat().read_number() == expected_number
+            
+    def test_read_string(self):
+        
+        # str of len 0
+        with mock.patch('peix.format.os', new=BytesMock(b'\x00' + junk)):
+            assert EixFileFormat().read_string() == ''
+
+        # str of len 3
+        with mock.patch('peix.format.os', new=BytesMock(b'\x03abc' + junk)):
+            assert EixFileFormat().read_string() == 'abc'
